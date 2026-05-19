9223160e5993a444b538a1f6b2e9a336c3fc75d9
batupenyu <babelhendri@gmail.com>
2026-05-19 11:00:42 +0700

    fix: suppress favicon.ico 404

--- AppAk2/urls.py
+++ AppAk2/urls.py
@@ -17,12 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pegawai/', include('pegawai.urls')),
+    path('favicon.ico', lambda req: HttpResponse(status=204)),
     path('', RedirectView.as_view(url='/pegawai/', permanent=True)),
 ]
 
