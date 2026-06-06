01424c74a02fa10f1479b75ebef5d22512e9fb5f
batupenyu <babelhendri@gmail.com>
2026-06-06 09:36:35 +0700

    Set DEBUG default to True

--- AppAk2/settings.py
+++ AppAk2/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('DJANGO_SECRET_KEY', default='django-insecure-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 # Allow Vercel domains and localhost
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'app-ak.vercel.app', '*.vercel.app', '*']
